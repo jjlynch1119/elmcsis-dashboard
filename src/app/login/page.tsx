3961f539e89417e401fb5eaed334aecfbdd11528
Ben Schiewe <ben.schiewe@gmail.com>
2025-03-28 16:48:40 -0500

    update login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,38 +1,19 @@
-import React from "react";
-import "@/styles/globals.css";
-import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button";
+import { GalleryVerticalEnd } from "lucide-react"
+
+import { LoginForm } from "@/components/login-form"
 
 export default function LoginPage() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-gray-100">
-      
-
-      <div className="w-full max-w-md p-8 bg-white shadow-md rounded-md">
-        <h1 className="text-2xl font-bold text-gray-500 text-center mb-6">Login</h1>
-        <form className="space-y-4">
-          <div>
-            <label htmlFor="username" className="block text-sm font-medium text-gray-700">
-              Username
-            </label>
-            <Input id="username" type="text" placeholder="Enter your username" className="text-gray-600" />
+    <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+      <div className="flex w-full max-w-sm flex-col gap-6">
+        <a href="#" className="flex items-center gap-2 self-center font-medium">
+          <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
+            <GalleryVerticalEnd className="size-4" />
           </div>
-          <div>
-            <label htmlFor="password" className="block text-sm font-medium text-gray-700">
-              Password
-            </label>
-            <Input id="password" type="password" placeholder="Enter your password" className="text-gray-600" />
-          </div>
-          <Button type="submit" className="w-full">
-            Login
-          </Button>
-        </form>
-        <div className="mt-4 text-center">
-          <Button variant="link" className="text-sm">
-            Forgot Password?
-          </Button>
-        </div>
+          Acme Inc.
+        </a>
+        <LoginForm />
       </div>
     </div>
-  );
+  )
 }
